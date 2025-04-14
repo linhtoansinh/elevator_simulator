71f00dfced334b17315020b4882c76cda41c69e4
Linh <linhtoansinh@gmail.com>
2025-04-14 12:00:24 +0700

    fix: remove unused celery

--- elevator_simulator/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elevator_simulator.settings')
-
-app = Celery('elevator_simulator')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
