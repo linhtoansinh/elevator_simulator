84bfee252f3a0eb8fd5303325e1f6554077844c3
Linh <linhtoansinh@gmail.com>
2025-04-15 12:11:34 +0700

    chore: add mongo volume

--- elevator/management/commands/populate_elevators.py
+++ elevator/management/commands/populate_elevators.py
@@ -3,30 +3,36 @@ from django.core.management.base import BaseCommand, CommandError
 from elevator.constants import NUM_ELEVATORS
 from elevator.models.elevator_mongo import Elevator
 from elevator.database import db
+from pymongo.errors import OperationFailure
 
 class Command(BaseCommand):
     help = "Populate new elevators to mongodb"
 
     def handle(self, *args, **options):
+        existed = True
         try:
-            collection = db["elevators"]
+            db.validate_collection("elevators")
+        except OperationFailure:
+            existed = False
 
-            collection.drop()
-            db.create_collection("elevators")
+        if not existed:
+            try:
+                db.create_collection("elevators")
+                collection = db["elevators"]
 
-            for i in range(NUM_ELEVATORS):
-                elevator = {
-                    "elevator_id": i,
-                    "current_floor": 1,
-                    "direction": None,
-                    "is_open": False,
-                    "target_floors": [],
-                    "is_moving":  False
-                }
-                collection.insert_one(elevator)
+                for i in range(NUM_ELEVATORS):
+                    elevator = {
+                        "elevator_id": i,
+                        "current_floor": 1,
+                        "direction": None,
+                        "is_open": False,
+                        "target_floors": [],
+                        "is_moving":  False
+                    }
+                    collection.insert_one(elevator)
 
-            self.stdout.write(
+                self.stdout.write(
                     self.style.SUCCESS('Successfully')
                 )
-        except Exception as e:
-            raise CommandError(e)
+            except Exception as e:
+                raise CommandError(e)
